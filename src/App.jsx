4861b15d88adcf4255515d1c6f72daadcdbada5b
RomuloReyes <reyes_romulo@hotmail.com>
2022-02-10 21:50:43 -0700

    correccion sintaxis

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import Modal from './components/Modal';
 import Filtros from './components/Filtros'
 import ListadoGastos from './components/ListadoGastos';
